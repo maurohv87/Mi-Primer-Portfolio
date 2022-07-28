5302734e5c87201f8ae27d4c03eb933f0205b1de
maurohv87 <maurohv_87@hotmail.com>
2022-07-28 00:09:56 -0300

    Creacion de carpeta BackEnd
    Se creo la carpeta BackEnd y se puso todo adentro

---
+++ BackEnd/miprimerportfolio/src/main/java/com/portfolio/miprimerportfolio/MiprimerportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.miprimerportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiprimerportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiprimerportfolioApplication.class, args);
+	}
+
+}
---
+++ BackEnd/miprimerportfolio/src/test/java/com/portfolio/miprimerportfolio/MiprimerportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.miprimerportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiprimerportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
